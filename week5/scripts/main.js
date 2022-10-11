0005ad65cf6801716b8c843b2c055806c9f3951e
meixuanchen <chmx2022@gmail.com>
2022-10-10 17:47:48 -0700

    class activity 1

---
+++ week5/scripts/main.js
@@ -0,0 +1,21 @@
+function getNumber(){
+	let num;
+
+	do {
+		num = prompt("get a number");
+	} while (isNan(num));
+	return num;
+}
+
+let number = getNumber();
+let radius = document.querySelector("#radius");
+radius.innerText += `${number}`;
+
+function calculateArea(rad){
+	let area = Math.PI* Math.pow(rad,2);
+	return area;
+}
+
+let area = calculateArea(number);
+let result = document.querySelector("#result");
+result.innerText += `${area}`;
