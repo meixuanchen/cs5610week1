061cab4678299d0702830dbf607b032fc2de9748
meixuanchen <chmx2022@gmail.com>
2022-10-10 17:53:30 -0700

    class activity 2

--- week5/scripts/main.js
+++ week5/scripts/main.js
@@ -1,21 +1,35 @@
-function getNumber(){
-	let num;
+// function getNumber(){
+// 	let num;
 
-	do {
-		num = prompt("get a number");
-	} while (isNan(num));
-	return num;
+// 	do {
+// 		num = prompt("get a number");
+// 	} while (isNan(num));
+// 	return num;
+// }
+
+// let number = getNumber();
+// let radius = document.querySelector("#radius");
+// radius.innerText += `${number}`;
+
+// function calculateArea(rad){
+// 	let area = Math.PI* Math.pow(rad,2);
+// 	return area;
+// }
+
+// let area = calculateArea(number);
+// let result = document.querySelector("#result");
+// result.innerText += `${area}`;
+
+
+let shoppinglistElement = document.querySelector(".shopping");
+
+let shoppingItems = ["bread", "cheese", "green pepper"];
+
+function populateList(arr){
+	for (let item of arr){
+		let listItem = document.createElement("li");
+		listItem.innerText = item;
+		shoppinglistElement.appendChild(listItem);
+	}
 }
-
-let number = getNumber();
-let radius = document.querySelector("#radius");
-radius.innerText += `${number}`;
-
-function calculateArea(rad){
-	let area = Math.PI* Math.pow(rad,2);
-	return area;
-}
-
-let area = calculateArea(number);
-let result = document.querySelector("#result");
-result.innerText += `${area}`;
+populateList(shoppingItems);
